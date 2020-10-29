622c250f8051eb67112e34f3b516012bb71e9a03
Timo Zandbergen <zandbergentimo@gmail.com>
2020-10-29 09:53:44 +0100

    turtle

---
+++ 01 Turtle/IDLE.py
@@ -0,0 +1,10 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> turtle.setup(400, 400, 0, 0)
+>>> for i in range (5):
+	turtle.forward(200)
+	turtle.left(144)
+
+	
+>>> 
---
+++ 01 Turtle/README.md
@@ -0,0 +1,1 @@
+# Flex-python-extra Timo Zandbergen Sd1A
